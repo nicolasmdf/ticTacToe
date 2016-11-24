2fa10430a3f35b75dca7c04e9f27feab83e0e674
Nicolas Reboredo <nicolasmdf@gmail.com>
2016-11-24 14:14:14 -0300

    adding some functionality

--- js/index.js
+++ js/index.js
@@ -1,34 +1,108 @@
 $(document).ready(function() {
 	let circle = "O";
 	let cross = "X";	
+	let player, computer, turn;
+	let board = {
+		s1: "",
+		s2: "",
+		s3: "",
+		s4: "",
+		s5: "",
+		s6: "",
+		s7: "",
+		s8: "",
+		s9: ""
+	};
+	let winningConditions = [
+		["s1", "s2", "s3"], 
+		["s4", "s5", "s6"],
+		["s7", "s8", "s9"],
+		["s1", "s4", "s7"], 
+		["s2", "s5", "s8"], 
+		["s3", "s6", "s9"], 
+		["s1", "s5", "s9"], 
+		["s3", "s5", "s7"]
+	];
 
-	$("#s5").click(function() {
-		$("#e").html(circle);
-		$("#s5").addClass("animate");
-	});	
-		$("#s6").click(function() {
-		$("#e").html("");	
-		$("#s5").removeClass("animate");
-	});	
+	let computerMove = (square) => {
+		$("#" + square).html(computer);
+		$("#" + square).addClass("animate");
+		board[square] = computer;
+		winCheck(computer);
+	};
+	
+	let winCheck = (input) => {
+		winningConditions.forEach(condition => {
+			let checked = condition.filter((square) => board[square] == input);
+			if (checked.length === 3) {
+				if (input === player) {
+					reset();
+					window.alert("You won!");
+				} else {
+					reset();
+					window.alert("You lost!");
+				}
+			}
+		});
+	};
+	
+	let ai = () => {
+		if (board.s5 === "") {
+			computerMove("s5");
+		}
+		winningConditions.forEach(condition => {   //check to block
+			let checked = condition.filter(square => board[square] == player);
+			if (checked.length === 2) {
+				condition.forEach((s) => board[s] === "" ? computerMove(s) : console.log(s));
+			}
+		});
+		
+		
+	};
+	
+	//take everything back to 0
+	let reset = () => {
+		$("#circleInput").removeClass("animate");
+		$("#crossInput").removeClass("animate");
+		$(".square").html("&nbsp");
+		$(".square").removeClass("animate");
+		$("#circleInput").removeClass("animate");
+		$("#crossInput").removeClass("animate");
+		$(".modal").css("display", "initial");
+		board.s1 = "";
+		board.s2 = "";
+		board.s3 = "";
+		board.s4 = "";
+		board.s5 = "";
+		board.s6 = "";
+		board.s7 = "";
+		board.s8 = "";
+		board.s9 = "";
+	};
+
+	$(".square").click(function() {
+		if (board[event.target.id] === "") {
+			$("#" + event.target.id).html(player);
+			$("#" + event.target.id).addClass("animate");
+			board[event.target.id] = player;
+			winCheck(player);
+			ai();
+		}
+	});
+	
 	$("#crossInput").click(function() {
+		player = cross;
+		computer = circle;
 		$("#crossInput").addClass("animate");
 		$(".modal").fadeOut(1000);
 	});	
+	
 	$("#circleInput").click(function() {
+		player = circle;
+		computer = cross;
 		$("#circleInput").addClass("animate");
 		$(".modal").fadeOut(1000);
 	});	
-	$("#reset").click(function() {
-		$("#e").html("");
-		$(".modal").css("display", "initial");
-		$("#s5").removeClass("animate");
-	});
+	
+	$("#reset").click(reset);
 });
-
-/*As far as front goes what is left is:
--the modal box to choose x or o.
--final animation for loosing/drawing/restarting ???
--finishing touches on color, font, etc.
-
-Modal is working, though not done. I can now start working on the js part of this o.O
-*/
